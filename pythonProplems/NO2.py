8f8b40812db542ef7e9b50611ac6cd7b3ec86b1c
kindie <kindienega10@gmail.com>
2023-05-26 00:06:45 +0300

    question add

--- pythonProplems/NO2.py
+++ pythonProplems/NO2.py
@@ -1,3 +1,6 @@
+
+#  Write a flat method triangle() that computes the area of a triangle using its two formal parameters h and w, where h is the height and we is the length of the buses of the triangle.
+
 def triangle(h, w):
     """
     Compute the area of a triangle given its height and base length.
