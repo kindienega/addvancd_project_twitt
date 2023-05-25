b9e2907a74d5e8194afb8662bda82e27f27aeb91
kindie <kindienega10@gmail.com>
2023-05-25 23:51:15 +0300

    number one is solved

---
+++ pythonProplems/NO1.py
@@ -0,0 +1,17 @@
+def cube(x):
+    """
+    Returns the cube of the given value.
+
+    Args:
+        value (int): The value to cube.
+
+    Returns:
+        int: The cube of the given value.
+    """
+    return x ** 3
+
+# Example 
+x=4
+result = cube(x)
+print(result)
+
