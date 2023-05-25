d51247498b5b1b204be30969e622ed6d32905b2a
kindie <kindienega10@gmail.com>
2023-05-25 23:54:21 +0300

    number 3 is solved

---
+++ pythonProplems/NO3.py
@@ -0,0 +1,13 @@
+def rectangle(h, w):
+    """
+    Compute the area of a rectangle given its height (h) and width (w).
+    """
+    return h * w
+
+# Example usage:
+height = 5.0
+width = 10.0
+
+area = rectangle(height, width)
+
+print(f"The area of a rectangle with height {height} and width {width} is: {area}")
